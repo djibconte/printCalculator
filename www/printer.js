13b941a3fad4df969e146dbecbd0c2a9f4c8c0dc
djibconte <44382290+djibconte@users.noreply.github.com>
2018-10-23 00:14:12 +0000

    Update printer.js

--- www/printer.js
+++ www/printer.js
@@ -1,6 +1,6 @@
 var exec = require('cordova/exec');
 
-exports.printMethod = function (arg0, success, error)
+module.exports.printMethod = function (arg0, success, error)
 module.exports.printMethod = function (arg0, success, error)
 {
     exec(success, error, 'printer', 'printMethod', [arg0]);
